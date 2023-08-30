cf04bbb47f2ab3fd5953c5db6a0e608a3620c804
caio <macbook@MacBook-Pro-de-Macbook.local>
2023-08-30 17:36:18 -0300

    fix port 2

--- src/services/base.tsx
+++ src/services/base.tsx
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 const renderBase: AxiosInstance = axios.create({
   // baseURL: "http://192.168.1.104:3333",
-  baseURL: "http://13.58.121.75/pegpag-api"
+  baseURL: "http://18.189.26.38/pegpag-api"
 });
 
 export default renderBase;
